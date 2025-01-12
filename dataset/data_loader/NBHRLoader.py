a91d0037a8f6f36b0ad4079cec0da334b5147bfe
A. Kamesh Anuradha <98675082+askanuradha@users.noreply.github.com>
2025-01-13 02:14:00 +0530

    debug

--- dataset/data_loader/NBHRLoader.py
+++ dataset/data_loader/NBHRLoader.py
@@ -76,7 +76,7 @@ class NBHRLoader(BaseLoader):
         hr_bvps = BaseLoader.resample_ppg(hr_bvps, frames.shape[0])
         spo2_bvps = BaseLoader.resample_ppg(spo2_bvps, frames.shape[0])
             
-        frames_clips, hr_bvps_clips, spo2_bvps_clips = self.preprocess(frames, hr_bvps, spo2_bvps, config_preprocess)
+        frames_clips, hr_bvps_clips, spo2_bvps_clips = self.preprocess(frames, hr_bvps, spo2_bvps, config_preprocess, filename)
         input_name_list, label_name_list = self.save_multi_process(frames_clips, hr_bvps_clips, spo2_bvps_clips, saved_filename)
         file_list_dict[i] = input_name_list
 
